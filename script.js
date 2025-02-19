ac23ee183fa5e3767bc2daec5814d56549b1beb6
Anujpanwar109 <166197689+Anujpanwar109@users.noreply.github.com>
2025-02-19 10:13:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,18 +7,18 @@ for(let i=0;i<boxes.length;i++){
 		
 		if(event.key==="Backspace"&& i>0){
 			 event.preventDefault();
-			boxes[i-1].focus();
+			boxes[i-1].focused();
 			boxes[i].value=""
 		}
 		else if(event.key==="Backspace" && i===0){
 				event.preventDefault();
-				boxes[i].focus();
+				boxes[i].focused();
 				boxes[i].value=""
 		}
 		else{
 			boxes[i].addEventListener("input",()=>{
 				if(i<boxes.length-1){
-				boxes[i+1].focus();
+				boxes[i+1].focused();
 	}
 })
 		}
