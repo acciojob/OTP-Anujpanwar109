c458b5328e70d952eeb05578c83bbb4bc1c79a74
Anujpanwar109 <166197689+Anujpanwar109@users.noreply.github.com>
2025-02-19 10:23:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,20 +5,20 @@ for(let i=0;i<boxes.length;i++){
 	boxes[i].addEventListener("keydown",(event)=>{
 
 		
-		if(event.key==="Backspace"&& i>0){
+		if(event.key==="Backspace"&& i>0 ){
 			 event.preventDefault();
-			boxes[i-1].focused();
+			boxes[i-1].focus();
 			boxes[i].value=""
 		}
-		else if(event.key==="Backspace" && i===0){
+		else if(event.key==="Backspace" && i===0 ){
 				event.preventDefault();
-				boxes[i].focused();
+				boxes[i].focus();
 				boxes[i].value=""
 		}
 		else{
 			boxes[i].addEventListener("input",()=>{
-				if(i<boxes.length-1){
-				boxes[i+1].focused();
+				if(i<boxes.length-1 && boxes[i].value !== ""){
+				boxes[i+1].focus();
 	}
 })
 		}
